7bca80b882d8327a227fb4579c46c23cf2d3c2ed
Wamula Bashir Saidi <wamulabash1@gmail.com>
2023-09-21 04:00:20 +0300

    working on deployment

--- UI/vite.config.js
+++ UI/vite.config.js
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig(() => {
     return {
+        base: process.env.NODE_ENV === 'production' ? '' : '/',
         plugins: [vue()],
         resolve: {
             alias: {
