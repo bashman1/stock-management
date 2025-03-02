418f4c75f72225d1fbd925a70fdc75ff162d036e
Wamula Bashir Saidi <wamulabash1@gmail.com>
2025-03-02 10:26:26 +0300

    worked on the password reset

---
+++ database/migrations/2025_03_02_101141_add_location_to_system_logs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('system_logs', function (Blueprint $table) {
+            $table->string('country')->nullable();
+            $table->string('city')->nullable();
+            $table->string('asn')->nullable();
+            $table->string('region')->nullable();
+            $table->string('latitude')->nullable();
+            $table->string('longitude')->nullable();
+            $table->string('postal_code')->nullable();
+            $table->string('isp')->nullable();
+            $table->string('time_zone')->nullable();
+            $table->uuid('uuid')->default(DB::raw('gen_random_uuid()'))->unique()->after('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('system_logs', function (Blueprint $table) {
+            //
+        });
+    }
+};
